287fff97c15ffd31e8d3b2fcfdd94781c77d6bd6
ymm <geekreading@163.com>
2023-11-20 13:22:47 +0800

    init: 初始化项目

--- server/indexImages.ts
+++ server/indexImages.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable dot-notation */
 import * as dotenv from "dotenv";
-import { Pinecone } from '@pinecone-database/pinecone';
-import type { PineconeRecord } from '@pinecone-database/pinecone';
+import { Pinecone } from "@pinecone-database/pinecone";
+import type { PineconeRecord } from "@pinecone-database/pinecone";
 import { embedder } from "./embeddings.ts";
 import { getEnv, listFiles } from "./utils/util.ts";
 import { chunkedUpsert } from "./utils/chunkedUpsert.ts";
@@ -11,8 +11,10 @@ dotenv.config();
 
 // Index setup
 const indexName = getEnv("PINECONE_INDEX");
-const pinecone = new Pinecone();
-
+const pinecone = new Pinecone({
+  apiKey: getEnv("PINECONE_API_KEY"),
+  environment: getEnv("PINECONE_ENVIRONMENT"),
+});
 
 function* chunkArray<T>(array: T[], chunkSize: number): Generator<T[]> {
   for (let i = 0; i < array.length; i += chunkSize) {
@@ -20,42 +22,51 @@ function* chunkArray<T>(array: T[], chunkSize: number): Generator<T[]> {
   }
 }
 
-
-async function embedAndUpsert({ imagePaths, chunkSize }: { imagePaths: string[], chunkSize: number }) {
+async function embedAndUpsert({
+  imagePaths,
+  chunkSize,
+}: {
+  imagePaths: string[];
+  chunkSize: number;
+}) {
   // Chunk the image paths into batches of size chunkSize
   const chunkGenerator = chunkArray(imagePaths, chunkSize);
 
   // Get the index
   const index = pinecone.index(indexName);
+  console.log(index);
 
   // Embed each batch and upsert the embeddings into the index
   for await (const imagePaths of chunkGenerator) {
-    await embedder.embedBatch(imagePaths, chunkSize, async (embeddings: PineconeRecord[]) => {
-      await chunkedUpsert(index, embeddings, "default");
-    });
+    await embedder.embedBatch(
+      imagePaths,
+      chunkSize,
+      async (embeddings: PineconeRecord[]) => {
+        await chunkedUpsert(index, embeddings);
+      }
+    );
   }
 }
 
 const indexImages = async () => {
   try {
     // Create the index if it doesn't already exist
-    const indexList = await pinecone.listIndexes();
-    if (indexList.indexOf({ name: indexName }) === -1) {
-      await pinecone.createIndex({ name: indexName, dimension: 512, waitUntilReady: true })
+    const indexList = (await pinecone.listIndexes()).map((index) => index.name);
+    if (!indexList.includes(indexName)) {
+      await pinecone.createIndex({
+        name: indexName,
+        dimension: 512,
+        waitUntilReady: true,
+      });
     }
-
     await embedder.init("Xenova/clip-vit-base-patch32");
     const imagePaths = await listFiles("./data");
     await embedAndUpsert({ imagePaths, chunkSize: 100 });
     return;
-
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
-export {
-  indexImages
-};
-
+export { indexImages };
--- server/query.ts
+++ server/query.ts
@@ -1,35 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { Pinecone } from "@pinecone-database/pinecone";
 import { embedder } from "./embeddings.ts";
 import { getEnv } from "./utils/util.ts";
-import { Pinecone } from '@pinecone-database/pinecone';
 
 type Metadata = {
   imagePath: string;
-}
+};
 
 const indexName = getEnv("PINECONE_INDEX");
-const pinecone = new Pinecone();
+const pinecone = new Pinecone({
+  apiKey: getEnv("PINECONE_API_KEY"),
+  environment: getEnv("PINECONE_ENVIRONMENT"),
+});
 const index = pinecone.index<Metadata>(indexName);
 
 await embedder.init("Xenova/clip-vit-base-patch32");
 
 const queryImages = async (imagePath: string) => {
   const queryEmbedding = await embedder.embed(imagePath);
-  const queryResult = await index.namespace('default').query({
-      vector: queryEmbedding.values,
-      includeMetadata: true,
-      includeValues: true,
-      topK: 6
+  const queryResult = await index.query({
+    vector: queryEmbedding.values,
+    includeMetadata: true,
+    includeValues: true,
+    topK: 6,
   });
-  return queryResult.matches?.map(match => {
-    const metadata = match.metadata;
+  return queryResult.matches?.map((match) => {
+    const { metadata } = match;
     return {
-      src: metadata ? metadata.imagePath : '',
-      score: match.score
-    };  
+      src: metadata ? metadata.imagePath : "",
+      score: match.score,
+    };
   });
 };
 
-export {
-  queryImages
-};
+export { queryImages };
